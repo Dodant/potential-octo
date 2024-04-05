3cdd733cc75eebf48508bf90e5cef4143a4b7680
Maestr Oh <ohho0506@gmail.com>
2024-04-05 19:26:41 +0900

    [Silver III] Title: N과 M (7), Time: 2208 ms, Memory: 31252 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+ls = sorted(list(map(int, input().split())))
+s = []
+
+def dfs():
+    if len(s) == m:
+        print(*s)
+        return 
+    for i in ls:
+        s.append(i)
+        dfs()
+        s.pop()
+
+dfs()
